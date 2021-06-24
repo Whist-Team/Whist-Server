e18736690021766488d46da7fa1292472bbf4477
Segelzwerg <marcel.haas@hhu.de>
2021-06-24 20:15:01 +0200

    ADD: test for user creation

---
+++ tests/whist/server/routes/user/test_create.py
@@ -0,0 +1,19 @@
+import unittest
+
+from starlette.testclient import TestClient
+
+from whist.server import app
+
+
+class UserTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(app)
+
+    def test_post_user(self):
+        """
+        Test the index route returns the game whist.
+        """
+        data = {'username': 'test'}
+        response = self.client.post(url='/user/create/', json=data)
+        self.assertEqual(response.status_code, 200, msg=response.content)
+        self.assertDictEqual(response.json(), {'user_id': '1'})

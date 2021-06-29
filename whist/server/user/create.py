358f22f209f5f36a957c819eac0c7b439b071a47
Segelzwerg <marcel.haas@hhu.de>
2021-06-29 21:49:01 +0200

    WIP: data base insertion

--- whist/server/user/create.py
+++ whist/server/user/create.py
@@ -1,16 +1,20 @@
 """'/user/create routes"""
+from typing import Dict
 
 from fastapi import APIRouter
-from whist.core.user.user import User
+
+from whist.server.db import db
+from whist.server.models.user import User
 
 router = APIRouter(prefix='/user/create')
 
 
 @router.post('/')
-def create_user(username: str):
+def create_user(request: Dict[str, str]):
     """
     Creates a new user.
     :return: the ID of the user or an error message.
     """
-    user = User(username)
-    return user.user_id
+    user = User(username=request['username'])
+    db.insert_one(user)
+    return {'user_id': '1'}

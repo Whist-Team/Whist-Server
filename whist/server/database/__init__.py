81d00747e6f603d71613c150e32677298fffd592
Segelzwerg <marcel.haas@hhu.de>
2021-07-05 21:52:27 +0200

    REFACTOR: extract connection

--- whist/server/database/__init__.py
+++ whist/server/database/__init__.py
@@ -1,12 +1,7 @@
-from pymongo import MongoClient
-
-from whist.server.const import DATABASE_NAME, TEST_DATABASE
+from whist.server.database.connection import get_database
 from whist.server.database.game_info import GameInfo
 
-client = MongoClient()
-if DATABASE_NAME == TEST_DATABASE:
-    client.drop_database(DATABASE_NAME)
-db = client[DATABASE_NAME]
+db = get_database()
 
 game_info = GameInfo(game='whist', version='0.1.0rc2')
 db.info.insert_one(game_info.dict(by_alias=True))
---
+++ whist/server/database/connection.py
@@ -0,0 +1,11 @@
+import pymongo
+
+from whist.server.const import DATABASE_NAME, TEST_DATABASE
+
+
+def get_database():
+    global db
+    client = pymongo.MongoClient()
+    if DATABASE_NAME == TEST_DATABASE:
+        client.drop_database(DATABASE_NAME)
+    return client[DATABASE_NAME]

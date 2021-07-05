a6d869427ba19c84f04f96821fe85fa340b0942d
Segelzwerg <marcel.haas@hhu.de>
2021-07-05 21:25:44 +0200

    ADD: game info data

--- whist/server/database/__init__.py
+++ whist/server/database/__init__.py
@@ -1,6 +1,12 @@
 from pymongo import MongoClient
 
-from whist.server.const import DATABASE_NAME
+from whist.server.const import DATABASE_NAME, TEST_DATABASE
+from whist.server.database.game_info import GameInfo
 
 client = MongoClient()
+if DATABASE_NAME == TEST_DATABASE:
+    client.drop_database(DATABASE_NAME)
 db = client[DATABASE_NAME]
+
+game_info = GameInfo(game='whist', version='0.1.0rc2')
+db.info.insert_one(game_info.dict(by_alias=True))

936acc5483251bf48b227b264ac2963a284abe64
Segelzwerg <marcel.haas@hhu.de>
2021-07-05 21:41:20 +0200

    ADD: mock mongo

--- tests/whist/server/routes/test_index.py
+++ tests/whist/server/routes/test_index.py
@@ -1,10 +1,12 @@
 import unittest
 
+import mongomock
 from fastapi.testclient import TestClient
 
 from whist.server import app
 
 
+@mongomock.patch(servers='mongodb://localhost:27017')
 class IndexTestCase(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(app)
@@ -13,6 +15,7 @@ class IndexTestCase(unittest.TestCase):
         """
         Test the index route returns the game whist.
         """
+
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertDictEqual(response.json(), {'game': 'whist'})
+        self.assertDictEqual(response.json(), {'info': {'game': 'whist', 'version': '1.0.0'}})

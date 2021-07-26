7b85fff861e7d87770a5d8672b74ce8275e42086
Segelzwerg <marcel.haas@hhu.de>
2021-07-26 21:15:07 +0200

    ADD: test for password service

---
+++ tests/whist/server/services/test_password.py
@@ -0,0 +1,17 @@
+import unittest
+
+from whist.server.services.password import PasswordService
+
+PASSWORD = 'abc'
+
+
+class PasswordServiceTestCase(unittest.TestCase):
+    def setUp(self):
+        self.pwd_service = PasswordService()
+        self.hashed_password = self.pwd_service.hash(PASSWORD)
+
+    def test_verify_true(self):
+        self.assertTrue(self.pwd_service.verify(PASSWORD, self.hashed_password))
+
+    def test_verify_false(self):
+        self.assertFalse(self.pwd_service.verify(PASSWORD, 'a'))

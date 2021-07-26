bd65f4b8885f336b54b188a82cb2362947f15c92
Segelzwerg <marcel.haas@hhu.de>
2021-07-26 21:57:01 +0200

    UPDATE: pwd service

--- whist/server/user/create.py
+++ whist/server/user/create.py
@@ -4,7 +4,8 @@ from typing import Dict
 from fastapi import APIRouter
 
 from whist.server.database import db
-from whist.server.database.user import User
+from whist.server.database.user import UserInDb
+from whist.server.services.password import PasswordService
 
 router = APIRouter(prefix='/user/create')
 
@@ -15,6 +16,9 @@ def create_user(request: Dict[str, str]):
     Creates a new user.
     :return: the ID of the user or an error message.
     """
-    user = User(username=request['username'])
+    pwd_service = PasswordService()
+    pwd_hash = pwd_service.hash(request['password'])
+    user = UserInDb(username=request['username'],
+                    hashed_password=pwd_hash)
     db.user.insert_one(user.dict(by_alias=True))
     return {'user_id': '1'}

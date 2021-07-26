fec7471a612b2fdc0828b37d5f2cc5abecf931c7
Segelzwerg <marcel.haas@hhu.de>
2021-07-26 21:57:31 +0200

    ADD: password in request

--- tests/whist/server/routes/user/test_create.py
+++ tests/whist/server/routes/user/test_create.py
@@ -13,7 +13,7 @@ class UserTestCase(unittest.TestCase):
         """
         Tests the creation of a new user.
         """
-        data = {'username': 'test'}
+        data = {'username': 'test', 'password': 'abc'}
         response = self.client.post(url='/user/create/', json=data)
         self.assertEqual(response.status_code, 200, msg=response.content)
         self.assertDictEqual(response.json(), {'user_id': '1'})

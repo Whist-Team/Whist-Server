e2c327de0744fe9eb31c7fd4bc9067abb9b224e3
Segelzwerg <marcel.haas@hhu.de>
2021-07-26 22:12:13 +0200

    ADD: test for no password in request

--- tests/whist/server/routes/user/test_create.py
+++ tests/whist/server/routes/user/test_create.py
@@ -3,11 +3,15 @@ import unittest
 from starlette.testclient import TestClient
 
 from whist.server import app
+from whist.server.database import db
 
 
 class UserTestCase(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(app)
+
+    def tearDown(self) -> None:
+        db.user.drop()
 
     def test_post_user(self):
         """
@@ -17,3 +21,13 @@ class UserTestCase(unittest.TestCase):
         response = self.client.post(url='/user/create/', json=data)
         self.assertEqual(response.status_code, 200, msg=response.content)
         self.assertDictEqual(response.json(), {'user_id': '1'})
+        self.assertEqual(1, db.user.count())
+
+    def test_post_user_no_pwd(self):
+        """
+        Tests the creation of a new user.
+        """
+        data = {'username': 'test'}
+        with self.assertRaisesRegex(KeyError, 'password'):
+            _ = self.client.post(url='/user/create/', json=data)
+        self.assertEqual(0, db.user.count())

76c9fbad52bbe79d44fa1e058de6af5d51997df4
Segelzwerg <marcel.haas@hhu.de>
2021-07-26 21:16:05 +0200

    ADD: User DO

--- whist/server/database/user.py
+++ whist/server/database/user.py
@@ -3,8 +3,19 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 from whist.server.database.id_wrapper import PyObjectId
+from whist.server.services.password import PasswordService
 
 
 class User(BaseModel):
     id: Optional[PyObjectId] = Field(alias='_id')
     username: str
+
+
+class UserInDb(User):
+    """
+    User DO
+    """
+    hashed_password: str
+
+    def verify_password(self, password):
+        return PasswordService.verify(password, self.hashed_password)

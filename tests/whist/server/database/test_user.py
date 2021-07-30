8aa915bc1bccd2f5c937b1b3a00b04c07fd9920b
Segelzwerg <marcel.haas@hhu.de>
2021-07-30 17:57:55 +0200

    ADD: test for password verification

---
+++ tests/whist/server/database/test_user.py
@@ -0,0 +1,16 @@
+import unittest
+
+from whist.server.database.user import UserInDb
+from whist.server.services.password import PasswordService
+
+
+class UserInDbTestCase(unittest.TestCase):
+    def setUp(self):
+        password_service = PasswordService()
+        self.user = UserInDb(username='test', hashed_password=password_service.hash('abc'))
+
+    def test_verify_pwd(self):
+        self.assertTrue(self.user.verify_password('abc'))
+
+    def test_verify_fail(self):
+        self.assertFalse(self.user.verify_password('bac'))

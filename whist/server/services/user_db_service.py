7006c4a903423a2423a598ca6f928c50f7d94aac
Segelzwerg <marcel.haas@hhu.de>
2021-08-08 21:12:23 +0200

    ADD: user database service

---
+++ whist/server/services/user_db_service.py
@@ -0,0 +1,38 @@
+"""User database connector."""
+from whist.server.database import db
+from whist.server.database.user import UserInDb
+
+
+class UserDatabaseService:
+    """
+    Handles interaction with user database.
+    """
+    _instance = None
+    _users = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(UserDatabaseService, cls).__new__(cls)
+            cls._users = db.user
+        return cls._instance
+
+    @classmethod
+    def add(cls, user: UserInDb) -> str:
+        """
+        Adds an user to the database.
+        :param user: to be added
+        :return: The id of the successful added user.
+        """
+        user_dict: dict = user.dict()
+        user_dict.pop('id')
+        user_id = cls._users.insert_one(user_dict)
+        return user_id.inserted_id
+
+    @classmethod
+    def get(cls, user_id) -> UserInDb:
+        """
+        Retrieves an user from the database.
+        :param user_id: of the user
+        :return: the user database object
+        """
+        return UserInDb(**cls._users.find_one(user_id))

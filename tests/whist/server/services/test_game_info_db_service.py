071d81978d55ff4883102137f84bf9a776c94f49
Segelzwerg <marcel.haas@hhu.de>
2021-08-08 22:33:12 +0200

    FIX: test not for id

--- tests/whist/server/services/test_game_info_db_service.py
+++ tests/whist/server/services/test_game_info_db_service.py
@@ -8,7 +8,7 @@ from whist.server.services.game_info_db_service import GameInfoDatabaseService
 class GameInfoDatabaseServiceTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.service = GameInfoDatabaseService()
-        self.info = GameInfo(game='whist', version='v1.0.0')
+        self.info = GameInfo(game='whist', version='v1.0.0').copy(exclude={'id'})
 
     def test_add(self):
         self.service.add(self.info)

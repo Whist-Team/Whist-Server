6362b7f7a1d0793cc2cbce8df4d5dfc92476fe2d
Segelzwerg <marcel.haas@hhu.de>
2021-08-08 21:38:57 +0200

    ADD: test for user not found

--- tests/whist/server/services/test_user_db_service.py
+++ tests/whist/server/services/test_user_db_service.py
@@ -3,6 +3,7 @@ import unittest
 from bson import ObjectId
 
 from whist.server.database.user import UserInDb
+from whist.server.services.error import UserNotFoundError
 from whist.server.services.user_db_service import UserDatabaseService
 
 
@@ -15,3 +16,7 @@ class UserDbTestCase(unittest.TestCase):
         user_id = self.user_database_service.add(self.user)
         self.user.id = ObjectId(user_id)
         self.assertEqual(self.user, self.user_database_service.get(user_id))
+
+    def test_user_not_existing(self):
+        with self.assertRaises(UserNotFoundError):
+            self.user_database_service.get('1' * 24)

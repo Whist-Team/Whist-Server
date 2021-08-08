3a1fe6d447c867985d9bb5c1243f2fe99a267397
Segelzwerg <marcel.haas@hhu.de>
2021-08-08 19:17:44 +0200

    FIX: missing arguments

--- whist/server/api/game/create.py
+++ whist/server/api/game/create.py
@@ -1,9 +1,10 @@
 from typing import Dict
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from whist.server.database import db
 from whist.server.database.game import GameInDb
+from whist.server.services.password import PasswordService
 
 router = APIRouter(prefix='/game')
 
@@ -15,10 +16,30 @@ def create_game(request: Dict[str, str]):
     :param request: Must contain a 'game_name'. 'password' is optional.
     :return: the ID of the game instance.
     """
-    pwd_hash = request['password'] if request['password'] else None
-    game = GameInDb(game_name=request['game_name'],
+    pwd_service = PasswordService()
+    pwd_hash = _get_password(pwd_service, request)
+    game_name = _get_game_name(request)
+
+    game = GameInDb(game_name=game_name,
                     password=pwd_hash)
     game_dict: dict = game.dict()
     game_dict.pop('id')
     game_id = db.game.insert_one(game_dict)
     return {'game_id': str(game_id.inserted_id)}
+
+
+def _get_game_name(request):
+    try:
+        game_name = request['game_name']
+    except KeyError:
+        raise HTTPException(status_code=400, detail='"game_name" is required.')
+    return game_name
+
+
+def _get_password(pwd_service, request):
+    try:
+        password = request['password']
+        pwd_hash = pwd_service.hash(password)
+    except KeyError:
+        pwd_hash = None
+    return pwd_hash

6a36e9605b5df6c3553ff2e27703e89a04025c27
Segelzwerg <marcel.haas@hhu.de>
2021-08-10 22:46:55 +0200

    ADD: userdb to user

--- whist/server/database/user.py
+++ whist/server/database/user.py
@@ -19,10 +19,17 @@ class UserInDb(User):
     """
     hashed_password: str
 
-    def verify_password(self, password):
+    def verify_password(self, password) -> bool:
         """
         Verifies the password for a specific user.
         :param password: plain text of the password
         :return: True if verified else False
         """
         return PasswordService.verify(password, self.hashed_password)
+
+    def to_user(self) -> User:
+        """
+        Converts the DO to DAO.
+        :return: User with no password saved in object.
+        """
+        return User(**self.dict())

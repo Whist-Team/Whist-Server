bd78f5c21d7ef6e2c34996494449dc683610bc52
Manuelraa <manuel@bloodycrystals.de>
2021-08-28 22:42:15 +0200

    Update ci to new setup structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
-    
+
 with open('requirements/requirements.txt') as f:
     requirements = f.readlines() + ['whist-core~=0.1.0rc2']
 

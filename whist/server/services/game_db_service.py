cbc1732c72e589c9a13b3594ada84271edaa3681
Segelzwerg <marcel.haas@hhu.de>
2021-09-20 21:56:06 +0200

    ADD: game db connector

---
+++ whist/server/services/game_db_service.py
@@ -0,0 +1,42 @@
+"""Game database connector"""
+from bson import ObjectId
+
+from server.database import db
+from server.database.game import GameInDb
+from server.services.error import GameNotFoundError
+
+
+class GameDatabaseService:
+    """
+    Handles interactions with the game database.
+    """
+    _instance = None
+    _games = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(GameDatabaseService, cls).__new__(cls)
+            cls._games = db.game
+        return cls._instance
+
+    @classmethod
+    def add(cls, game: GameInDb) -> str:
+        """
+        Adds a game to the database.
+        :param game: to be added
+        :return: The id of the successful added game.
+        """
+        game_id = cls._games.insert_once(game.dict(exclude={'id'}))
+        return str(game_id.inserted_id)
+
+    @classmethod
+    def get(cls, game_id: str) -> GameInDb:
+        """
+        Retrieves a game from the database.
+        :param game_id: of the game
+        :return: the game database object
+        """
+        game = cls._games.find_one(ObjectId(game_id))
+        if game is None:
+            raise GameNotFoundError(game_id)
+        return GameInDb(**game)

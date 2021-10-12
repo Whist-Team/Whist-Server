7be0f410c3206a7386ab3916054c24b665d2de02
Segelzwerg <marcel.haas@hhu.de>
2021-10-12 20:57:52 +0200

    ADD: test for authentication route

---
+++ tests/whist/server/api/user/test_auth.py
@@ -0,0 +1,31 @@
+import unittest
+
+from starlette.testclient import TestClient
+
+from whist.server import app
+from whist.server.database import db
+
+
+class AuthTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = TestClient(app)
+        self.login_creds = {'username': 'test', 'password': 'abc'}
+        _ = self.client.post(url='/user/create/', json=self.login_creds)
+
+    def tearDown(self) -> None:
+        db.user.drop()
+
+    def test_auth_user(self):
+        response = self.client.post(url='/user/auth/', data=self.login_creds)
+        self.assertEqual(response.status_code, 200, msg=response.content)
+        self.assertTrue('token' in response.json())
+
+    def test_no_password(self):
+        response = self.client.post(url='/user/auth/', data={'username': 'marcel'})
+        self.assertEqual(response.status_code, 422, msg=response.content)
+        self.assertFalse('token' in response.json())
+
+    def test_no_username(self):
+        response = self.client.post(url='/user/auth/', data={'password': 'abc'})
+        self.assertEqual(response.status_code, 422, msg=response.content)
+        self.assertFalse('token' in response.json())

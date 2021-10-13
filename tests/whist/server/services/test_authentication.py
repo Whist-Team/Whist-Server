c1d5cb155c375c1a90455c40a96f2b6a8deeff3e
Segelzwerg <marcel.haas@hhu.de>
2021-10-13 20:55:51 +0200

    REFACTOR: extract user creation

--- tests/whist/server/services/test_authentication.py
+++ tests/whist/server/services/test_authentication.py
@@ -8,11 +8,17 @@ from whist.server.services.password import PasswordService
 from whist.server.services.user_db_service import UserDatabaseService
 
 
-@pytest.mark.asyncio
-async def test_get_current_user():
-    user = UserInDb(username='test', hashed_password='abc')
+def _create_user():
+    hashed_password = PasswordService().hash('abc')
+    user = UserInDb(username='test', hashed_password=hashed_password)
     user_db_service = UserDatabaseService()
     user.id = ObjectId(user_db_service.add(user))
+    return user
+
+
+@pytest.mark.asyncio
+async def test_get_current_user():
+    user = _create_user()
     token = create_access_token(data={'sub': user.username})
     result_user = await get_current_user(token)
     assert user.to_user() == result_user
@@ -20,9 +26,6 @@ async def test_get_current_user():
 
 @pytest.mark.asyncio
 async def test_check_credentials():
-    hashed_password = PasswordService().hash('abc')
-    user = UserInDb(username='test', hashed_password=hashed_password)
-    user_db_service = UserDatabaseService()
-    user.id = ObjectId(user_db_service.add(user))
+    _ = _create_user()
     is_valid = await check_credentials('test', 'abc')
     assert is_valid

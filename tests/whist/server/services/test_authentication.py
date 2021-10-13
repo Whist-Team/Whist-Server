00608cb47464b307cdd6d63a0b7851f97bf35cd7
Segelzwerg <marcel.haas@hhu.de>
2021-10-13 20:52:09 +0200

    ADD: test for check credentials

--- tests/whist/server/services/test_authentication.py
+++ tests/whist/server/services/test_authentication.py
@@ -2,7 +2,9 @@ import pytest
 from bson import ObjectId
 
 from whist.server.database.user import UserInDb
-from whist.server.services.authentication import get_current_user, create_access_token
+from whist.server.services.authentication import get_current_user, create_access_token, \
+    check_credentials
+from whist.server.services.password import PasswordService
 from whist.server.services.user_db_service import UserDatabaseService
 
 
@@ -14,3 +16,13 @@ async def test_get_current_user():
     token = create_access_token(data={'sub': user.username})
     result_user = await get_current_user(token)
     assert user.to_user() == result_user
+
+
+@pytest.mark.asyncio
+async def test_check_credentials():
+    hashed_password = PasswordService().hash('abc')
+    user = UserInDb(username='test', hashed_password=hashed_password)
+    user_db_service = UserDatabaseService()
+    user.id = ObjectId(user_db_service.add(user))
+    is_valid = await check_credentials('test', 'abc')
+    assert is_valid

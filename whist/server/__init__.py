87a9ae169fe8ef80b775ccddfae72822d2534e00
Segelzwerg <marcel.haas@hhu.de>
2021-10-14 23:29:41 +0200

    FIX: duplicate merge

--- whist/server/__init__.py
+++ whist/server/__init__.py
@@ -4,17 +4,15 @@ from fastapi import FastAPI
 
 from whist.server.api import api
 from whist.server.api.game.create import router as game_creation
-from whist.server.api.user import create, auth
+from whist.server.api.user import auth
 from whist.server.api.user.create import router as user_creation
 from whist.server.database.game_info import GameInfo
 from whist.server.services.game_info_db_service import GameInfoDatabaseService
-from .api import api
 
 app = FastAPI()
 app.include_router(api.router)
 app.include_router(game_creation)
 app.include_router(user_creation)
-app.include_router(auth.router)
 app.include_router(auth.router)
 
 whist_core_version = pkg_resources.get_distribution('whist-core').version

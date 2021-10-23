93860b3444473128e322cf5482b56fbaec14f224
Segelzwerg <marcel.haas@hhu.de>
2021-10-23 21:33:51 +0200

    FIX: test to check for id

--- tests/whist/server/database/test_user.py
+++ tests/whist/server/database/test_user.py
@@ -1,5 +1,6 @@
 import unittest
 
+from whist.server.database.id_wrapper import PyObjectId
 from whist.server.database.user import UserInDb, User
 from whist.server.services.password import PasswordService
 
@@ -7,7 +8,9 @@ from whist.server.services.password import PasswordService
 class UserInDbTestCase(unittest.TestCase):
     def setUp(self):
         password_service = PasswordService()
-        self.user = UserInDb(username='test', hashed_password=password_service.hash('abc'))
+        self.user = UserInDb(_id=PyObjectId('1' * 24), username='test',
+                             hashed_password=password_service.hash(
+                                 'abc'))
 
     def test_verify_pwd(self):
         self.assertTrue(self.user.verify_password('abc'))
@@ -16,5 +19,5 @@ class UserInDbTestCase(unittest.TestCase):
         self.assertFalse(self.user.verify_password('bac'))
 
     def test_user(self):
-        user = User(id=self.user.id, username=self.user.username)
+        user = User(_id=self.user.id, username=self.user.username)
         self.assertEqual(user, self.user.to_user())

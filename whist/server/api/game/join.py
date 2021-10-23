d8e25922327c0f9b03363befeb5c8d89814285d4
Segelzwerg <marcel.haas@hhu.de>
2021-10-23 20:54:16 +0200

    ADD: password check

--- whist/server/api/game/join.py
+++ whist/server/api/game/join.py
@@ -1,20 +1,35 @@
 from typing import Dict
 
-from fastapi import APIRouter, Security
+from fastapi import APIRouter, HTTPException, Security, status
 
 from whist.server.database.user import User
 from whist.server.services.authentication import get_current_user
+from whist.server.services.game_db_service import GameDatabaseService
+from whist.server.services.password import PasswordService
 
 router = APIRouter(prefix='/game')
 
 
 @router.post('/join/{game_id}', status_code=200)
-def join_game(request: Dict[str, str], user: User = Security(get_current_user)):
+def join_game(game_id: str, request: Dict[str, str], user: User = Security(get_current_user)):
     """
     User requests to join a game.
+    :param game_id: unique identifier for a game
     :param request: must contain the key 'password'
     :param user: that tries to join the game. Must be authenticated.
     :return: the status of the join request. 'joined' for successful join
     """
+    pwd_service = PasswordService()
 
+    game_pwd = request['password']
+
+    game_service = GameDatabaseService()
+    game = game_service.get(game_id)
+
+    if not pwd_service.verify(game_pwd, game.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Wrong game password.",
+            headers={"WWW-Authenticate": "Basic"},
+        )
     return {'status': 'joined'}

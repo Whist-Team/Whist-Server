5b13feb0dc99c70d3fc543baa85396504d66dbd6
Segelzwerg <marcel.haas@hhu.de>
2021-10-31 14:16:48 +0100

    FIX: interace changes of player being returned

--- tests/whist/server/database/test_game.py
+++ tests/whist/server/database/test_game.py
@@ -1,3 +1,5 @@
+from whist.core.user.player import Player
+
 from tests.whist.server.base_player_test_case import BasePlayerTestCase
 from whist.server.database.game import GameInDb
 from whist.server.database.warning import PlayerAlreadyJoinedWarning
@@ -5,13 +7,15 @@ from whist.server.services.password import PasswordService
 
 
 class GameInDbTestCase(BasePlayerTestCase):
+
     def setUp(self) -> None:
         super().setUp()
         password_service = PasswordService()
         self.game: GameInDb = GameInDb.create_with_pwd(game_name='test',
                                                        hashed_password=password_service.hash('abc'),
                                                        creator=self.player)
-        self.assertEqual(['1'], self.game.players)
+        self.second_player = Player(username='2', rating=1200)
+        self.expected_players = [self.player, self.second_player]
 
     def test_verify_pwd(self):
         self.assertTrue(self.game.verify_password('abc'))
@@ -24,11 +28,11 @@ class GameInDbTestCase(BasePlayerTestCase):
         self.assertTrue(game.verify_password(None))
 
     def test_join(self):
-        self.assertTrue(self.game.join('2'))
-        self.assertEqual(['1', '2'], self.game.players)
+        self.assertTrue(self.game.join(self.second_player))
+        self.assertEqual(self.expected_players, self.game.players)
 
     def test_join_twice(self):
-        self.assertTrue(self.game.join('2'))
+        self.assertTrue(self.game.join(self.second_player))
         with self.assertRaises(PlayerAlreadyJoinedWarning):
-            self.assertTrue(self.game.join('2'))
-        self.assertEqual(['1', '2'], self.game.players)
+            self.assertTrue(self.game.join(self.second_player))
+        self.assertEqual(self.expected_players, self.game.players)

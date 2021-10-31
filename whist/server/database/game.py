b13ea31f94142e9ef28ba8d827b9d0a71f4f61bc
Segelzwerg <marcel.haas@hhu.de>
2021-10-31 13:21:04 +0100

    UPDATE: add table to game class ADD: factory methods

--- whist/server/database/game.py
+++ whist/server/database/game.py
@@ -2,6 +2,8 @@
 from typing import Optional
 
 from pydantic import BaseModel, Field
+from whist.core.session.table import Table
+from whist.core.user.player import Player
 
 from whist.server.database.id_wrapper import PyObjectId
 from whist.server.services.password import PasswordService
@@ -15,8 +17,22 @@ class Game(BaseModel):
     creator: user id as string of the player how created that session.
     """
     id: Optional[PyObjectId] = Field(alias='_id')
-    game_name: str
-    creator: str
+    creator: Player
+    table: Table = None
+
+    @classmethod
+    def create(cls, game_name: str, creator: Player,
+               min_player: int = 4, max_player: int = 4, **kwargs) -> 'Game':
+        table = Table(name=game_name, min_player=min_player, max_player=max_player)
+        table.join(creator)
+        return Game(creator=creator, table=table)
+
+    @property
+    def game_name(self) -> str:
+        """
+        :return: name of the game.
+        """
+        return self.table.name
 
 
 class GameInDb(Game):
@@ -24,6 +40,12 @@ class GameInDb(Game):
     Game DO
     """
     hashed_password: Optional[str]
+
+    @classmethod
+    def create_with_pwd(cls, game_name: str, creator: Player, hashed_password: str,
+                        min_player: int = 4, max_player: int = 4) -> 'GameInDb':
+        game = super().create(game_name, creator, min_player, max_player)
+        return GameInDb(**game.dict(), hashed_password=hashed_password)
 
     def verify_password(self, password: Optional[str]):
         """

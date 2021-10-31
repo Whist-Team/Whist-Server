29c98346e6c92df756a8c1e3dbd819fa109b1ac6
Segelzwerg <marcel.haas@hhu.de>
2021-10-31 13:37:15 +0100

    UPDATE: use factory methods in test

--- tests/whist/server/database/test_game.py
+++ tests/whist/server/database/test_game.py
@@ -8,9 +8,9 @@ class GameInDbTestCase(BasePlayerTestCase):
     def setUp(self) -> None:
         super().setUp()
         password_service = PasswordService()
-        self.game: GameInDb = GameInDb(game_name='test',
-                                       hashed_password=password_service.hash('abc'),
-                                       creator=self.player)
+        self.game: GameInDb = GameInDb.create_with_pwd(game_name='test',
+                                                       hashed_password=password_service.hash('abc'),
+                                                       creator=self.player)
         self.assertEqual(['1'], self.game.players)
 
     def test_verify_pwd(self):
@@ -20,7 +20,7 @@ class GameInDbTestCase(BasePlayerTestCase):
         self.assertFalse(self.game.verify_password('bac'))
 
     def test_verify_without_password(self):
-        game: GameInDb = GameInDb(game_name='test', creator=self.player)
+        game: GameInDb = GameInDb.create_with_pwd(game_name='test', creator=self.player)
         self.assertTrue(game.verify_password(None))
 
     def test_join(self):

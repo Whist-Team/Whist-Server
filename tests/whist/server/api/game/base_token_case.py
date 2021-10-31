e10c1b128109fbbc040fec17ce084bdf2cff0f9e
Segelzwerg <marcel.haas@hhu.de>
2021-10-31 14:15:43 +0100

    REFACTOR: extract create and authorization method

--- tests/whist/server/api/game/base_token_case.py
+++ tests/whist/server/api/game/base_token_case.py
@@ -9,10 +9,13 @@ from whist.server.database import db
 class TestCaseWithToken(unittest.TestCase):
     def setUp(self) -> None:
         self.client = TestClient(app)
-        self.login_creds = {'username': 'marcel', 'password': 'abc'}
-        _ = self.client.post(url='/user/create', json=self.login_creds)
-        token = self.client.post(url='/user/auth/', data=self.login_creds).json()['token']
-        self.headers = {'Authorization': f'Bearer {token}'}
+        self.headers = self.create_and_auth_user('marcel', 'abc')
+
+    def create_and_auth_user(self, user: str, password):
+        login_creds = {'username': user, 'password': password}
+        _ = self.client.post(url='/user/create', json=login_creds)
+        token = self.client.post(url='/user/auth/', data=login_creds).json()['token']
+        return {'Authorization': f'Bearer {token}'}
 
     def tearDown(self) -> None:
         db.game.drop()

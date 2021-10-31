c529c00b3136cf6b399f255c7d29b13adb4f2137
Segelzwerg <marcel.haas@hhu.de>
2021-10-31 13:34:32 +0100

    UPDATE: make pylint happy

--- whist/server/database/game.py
+++ whist/server/database/game.py
@@ -22,7 +22,15 @@ class Game(BaseModel):
 
     @classmethod
     def create(cls, game_name: str, creator: Player,
-               min_player: int = 4, max_player: int = 4, **kwargs) -> 'Game':
+               min_player: int = 4, max_player: int = 4) -> 'Game':
+        """
+        Factory method to create a Game object.
+        :param game_name: name of the this session
+        :param creator: player object of the host
+        :param min_player: the minimum amount of player to start a game
+        :param max_player: the maximum amount of player that can join this session
+        :return: the Game object
+        """
         table = Table(name=game_name, min_player=min_player, max_player=max_player)
         table.join(creator)
         return Game(creator=creator, table=table)
@@ -41,9 +49,19 @@ class GameInDb(Game):
     """
     hashed_password: Optional[str]
 
+    # pylint: disable=too-many-arguments
     @classmethod
     def create_with_pwd(cls, game_name: str, creator: Player, hashed_password: str,
                         min_player: int = 4, max_player: int = 4) -> 'GameInDb':
+        """
+        Factory method to create a Game in database object.
+        :param game_name: name of the this session
+        :param creator: player object of the host
+        :param hashed_password: the hash value of the password required to join
+        :param min_player: the minimum amount of player to start a game
+        :param max_player: the maximum amount of player that can join this session
+        :return: the Game object
+        """
         game = super().create(game_name, creator, min_player, max_player)
         return GameInDb(**game.dict(), hashed_password=hashed_password)
 

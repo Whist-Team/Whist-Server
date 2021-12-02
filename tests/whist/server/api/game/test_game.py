489a7791b81b5b7dd32cf3fb2b478e6e67499b3b
Segelzwerg <marcel.haas@hhu.de>
2021-12-02 09:25:09 +0100

    FIX: removed function to newer method

--- tests/whist/server/api/game/test_game.py
+++ tests/whist/server/api/game/test_game.py
@@ -23,18 +23,18 @@ class GameTestCase(unittest.TestCase):
         response = self.client.post(url='/game/create', json=data, headers=self.headers)
         self.assertEqual(response.status_code, 200, msg=response.content)
         self.assertTrue('game_id' in response.json())
-        self.assertEqual(1, db.game.count())
+        self.assertEqual(1, db.game.estimated_document_count())
 
     def test_post_game_without_pwd(self):
         data = {'game_name': 'test'}
         response = self.client.post(url='/game/create', json=data, headers=self.headers)
         self.assertEqual(response.status_code, 200, msg=response.content)
         self.assertTrue('game_id' in response.json())
-        self.assertEqual(1, db.game.count())
+        self.assertEqual(1, db.game.estimated_document_count())
 
     def test_post_game_without_name(self):
         data = {'password': 'abc'}
         response = self.client.post(url='/game/create', json=data, headers=self.headers)
         self.assertEqual(response.status_code, 400, msg=response.content)
         self.assertEqual('"game_name" is required.', response.json()['detail'])
-        self.assertEqual(0, db.game.count())
+        self.assertEqual(0, db.game.estimated_document_count())

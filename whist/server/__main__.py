fe9e0d245d8e490db4cb73d22f4c05c5587d8cc5
Segelzwerg <marcel.haas@hhu.de>
2021-12-08 22:03:15 +0100

    ADD: initial admin

--- whist/server/__main__.py
+++ whist/server/__main__.py
@@ -7,5 +7,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Whist Server')
     parser.add_argument('host_addr', type=str, help='Local address of the Whist Server.')
     parser.add_argument('host_port', type=int, help='Local port of the Whist Server.')
+    parser.add_argument('--admin_name', type=str, help="Admin's username")
+    parser.add_argument('--admin_pwd', type=str,
+                        help="Admin's inital password. Must be changed after deployment.")
     args = parser.parse_args()
     main(host=args.host_addr, port=args.host_port)

addf62d0441b40e1ceb5f18fd9e8782067b64900
Segelzwerg <marcel.haas@hhu.de>
2021-12-08 21:43:11 +0100

    ADD: cli arguments parsing

--- whist/server/__main__.py
+++ whist/server/__main__.py
@@ -1,5 +1,11 @@
 """Main entrypoint"""
+import argparse
+
 from whist.server.cli import main
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Whist Server')
+    parser.add_argument('host_addr', type=str, help='Local address of the Whist Server.')
+    parser.add_argument('host_port', type=int, help='Local port of the Whist Server.')
+    args = parser.parse_args()
+    main(host=args.host_addr, port=args.host_port)

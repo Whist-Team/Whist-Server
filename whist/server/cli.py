f5b526c897262802d1bf7785fb2f23b56fc3f1fe
Segelzwerg <marcel.haas@hhu.de>
2021-12-08 21:01:40 +0100

    REFACTOR: move variable to parameters

--- whist/server/cli.py
+++ whist/server/cli.py
@@ -6,6 +6,6 @@ from whist.server import app
 from whist.server.const import HOST_ADDR, HOST_PORT
 
 
-def main():
+def main(host=HOST_ADDR, port=HOST_PORT):
     """Main function."""
-    uvicorn.run(app, host=HOST_ADDR, port=HOST_PORT)
+    uvicorn.run(app, host=host, port=port)

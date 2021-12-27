aea51e48b3bdac1ddf237d06bfefc84991357a4d
Segelzwerg <marcel.haas@hhu.de>
2021-12-27 17:16:35 +0100

    ADD: test for join as requirement

--- tests/whist/server/api/game/test_action.py
+++ tests/whist/server/api/game/test_action.py
@@ -3,8 +3,16 @@ from tests.whist.server.api.game.base_created_case import BaseCreateGameTestCase
 
 class ActionGameTestCase(BaseCreateGameTestCase):
     def test_start(self):
+        _ = self.client.post(url=f'/game/join/{self.game_id}',
+                                    json={'password': 'abcd'},
+                                    headers=self.headers)
         response = self.client.post(url=f'/game/action/start/{self.game_id}',
                                     headers=self.headers)
         self.assertEqual(200, response.status_code, msg=response.content)
         self.assertEqual('started', response.json()['status'])
 
+    def test_unjoined(self):
+        response = self.client.post(url=f'/game/action/start/{self.game_id}',
+                                    headers=self.headers)
+        self.assertEqual(403, response.status_code, msg=response.content)
+        self.assertEqual('not started', response.json()['status'])

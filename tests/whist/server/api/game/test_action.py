6397bd343a61fc77a19368d112b5f8017d94776c
Segelzwerg <marcel.haas@hhu.de>
2021-12-29 13:56:32 +0100

    ADD: test for joining a table

--- tests/whist/server/api/game/test_action.py
+++ tests/whist/server/api/game/test_action.py
@@ -11,7 +11,21 @@ class ActionGameTestCase(BaseCreateGameTestCase):
         self.assertEqual(200, response.status_code, msg=response.content)
         self.assertEqual('started', response.json()['status'])
 
-    def test_unjoined(self):
+    def test_start_not_joined(self):
         response = self.client.post(url=f'/game/action/start/{self.game_id}',
                                     headers=self.headers)
+        self.assertEqual(403, response.status_code, msg=response.content)
+
+    def test_ready(self):
+        _ = self.client.post(url=f'/game/join/{self.game_id}',
+                             json={'password': 'abcd'},
+                             headers=self.headers)
+        response = self.client.post(url=f'/game/action/ready/{self.game_id}',
+                                    headers=self.headers)
+        self.assertEqual(200, response.status_code, msg=response.content)
+
+    def test_ready_not_joined(self):
+        headers = self.create_and_auth_user('miles', 'abc')
+        response = self.client.post(url=f'/game/action/ready/{self.game_id}',
+                                    headers=headers)
         self.assertEqual(403, response.status_code, msg=response.content)

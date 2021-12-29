7fe0cad9ca881719d9be1d6d7f2b6c9e23661a10
Segelzwerg <marcel.haas@hhu.de>
2021-12-29 12:47:26 +0100

    REFORMAT

--- tests/whist/server/api/game/test_action.py
+++ tests/whist/server/api/game/test_action.py
@@ -4,8 +4,8 @@ from tests.whist.server.api.game.base_created_case import BaseCreateGameTestCase
 class ActionGameTestCase(BaseCreateGameTestCase):
     def test_start(self):
         _ = self.client.post(url=f'/game/join/{self.game_id}',
-                                    json={'password': 'abcd'},
-                                    headers=self.headers)
+                             json={'password': 'abcd'},
+                             headers=self.headers)
         response = self.client.post(url=f'/game/action/start/{self.game_id}',
                                     headers=self.headers)
         self.assertEqual(200, response.status_code, msg=response.content)

808e8d6ae419bd1dfd5384a758cd20e08e05fc54
Segelzwerg <marcel.haas@hhu.de>
2021-12-30 12:29:55 +0100

    ADD: game save method

--- whist/server/services/game_db_service.py
+++ whist/server/services/game_db_service.py
@@ -3,7 +3,7 @@ from bson import ObjectId
 
 from whist.server.database import db
 from whist.server.database.game import GameInDb
-from whist.server.services.error import GameNotFoundError
+from whist.server.services.error import GameNotFoundError, GameNotUpdatedError
 
 
 class GameDatabaseService:
@@ -56,3 +56,14 @@ class GameDatabaseService:
         if game is None:
             raise GameNotFoundError(game_name=game_name)
         return GameInDb(**game)
+
+    @classmethod
+    def save(cls, game: GameInDb) -> None:
+        query = {'_id': ObjectId(game.id)}
+        values = {'$set': game.dict()}
+        result = cls._games.update_one(query, values)
+        if result.matched_count != 1:
+            raise GameNotFoundError(game.id)
+        if result.modified_count != 1:
+            raise GameNotUpdatedError(game.id)
+        print(result)

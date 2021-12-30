954bcdd1ba871ad437e4b433e118d08d0206f92f
Segelzwerg <marcel.haas@hhu.de>
2021-12-30 13:00:37 +0100

    ADD: test for non creator table start

--- tests/whist/server/api/game/test_action.py
+++ tests/whist/server/api/game/test_action.py
@@ -18,6 +18,24 @@ class ActionGameTestCase(BaseCreateGameTestCase):
         self.assertEqual(200, response.status_code, msg=response.content)
         self.assertEqual('started', response.json()['status'])
 
+    def test_start_not_creator(self):
+        # Create another user
+        headers = self.create_and_auth_user('miles', 'abc')
+
+        # Join new user
+        _ = self.client.post(url=f'/game/join/{self.game_id}',
+                             json={'password': 'abcd'},
+                             headers=headers)
+
+        # New user mark theyself ready
+        _ = self.client.post(url=f'/game/action/ready/{self.game_id}',
+                             headers=headers)
+
+        # New user tries to start table.
+        response = self.client.post(url=f'/game/action/ready/{self.game_id}',
+                                    headers=headers)
+        self.assertEqual(403, response.status_code, msg=response.content)
+
     def test_start_table_not_ready(self):
         # Join the player
         _ = self.client.post(url=f'/game/join/{self.game_id}',

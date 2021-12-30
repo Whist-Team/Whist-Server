565f1f0583987c1e2b8cd0b06259ef700dfb7237
Segelzwerg <marcel.haas@hhu.de>
2021-12-30 10:37:24 +0100

    ADD: documentation of setup posts

--- tests/whist/server/api/game/test_action.py
+++ tests/whist/server/api/game/test_action.py
@@ -3,9 +3,14 @@ from tests.whist.server.api.game.base_created_case import BaseCreateGameTestCase
 
 class ActionGameTestCase(BaseCreateGameTestCase):
     def test_start(self):
+        # Join the player
         _ = self.client.post(url=f'/game/join/{self.game_id}',
                              json={'password': 'abcd'},
                              headers=self.headers)
+        # Mark the player ready
+        _ = self.client.post(url=f'/game/action/ready/{self.game_id}',
+                             headers=self.headers)
+        # Request to start table.
         response = self.client.post(url=f'/game/action/start/{self.game_id}',
                                     headers=self.headers)
         self.assertEqual(200, response.status_code, msg=response.content)

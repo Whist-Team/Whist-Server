af4fe9bb25ffa5ee2d7dc98def48ff5dbeabb8be
Segelzwerg <marcel.haas@hhu.de>
2022-01-10 19:49:39 +0100

    ADD: test for suite serving warning

--- tests/whist/server/database/test_game.py
+++ tests/whist/server/database/test_game.py
@@ -1,5 +1,6 @@
 from whist.core.cards.card import Card, Suit, Rank
 from whist.core.cards.stack import Stack
+from whist.core.game.warnings import ServSuitFirstWarning
 from whist.core.user.player import Player
 
 from tests.whist.server.base_player_test_case import BasePlayerTestCase
@@ -70,4 +71,22 @@ class GameInDbTestCase(BasePlayerTestCase):
         expected_stack = Stack()
         expected_stack.add(first_card)
         expected_stack.add(second_card)
+        self.assertEqual(expected_stack, self.game.current_stack)
+
+    def test_play_illegal_card(self):
+        self.game.ready_player(self.player)
+        self.game.join(self.second_player)
+        self.game.ready_player(self.second_player)
+        self.game.start(self.player)
+        first_card = Card(Suit.CLUBS, Rank.A)
+        second_card = Card(Suit.HEARTS, Rank.K)
+        hand = self.game.current_rubber.next_game().next_hand()
+        play_order = hand._current_play_order._play_order
+        first_player = play_order[0].player
+        second_player = play_order[1].player
+        self.game.play_card(first_player, first_card)
+        expected_stack = Stack()
+        expected_stack.add(first_card)
+        with self.assertRaises(ServSuitFirstWarning):
+            self.game.play_card(second_player, second_card)
         self.assertEqual(expected_stack, self.game.current_stack)

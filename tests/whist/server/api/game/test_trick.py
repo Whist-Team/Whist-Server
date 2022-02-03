22fabeb854dfc6be76d0ad0ca805ae56c5fb59e9
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-02-03 22:10:08 +0100

    UPDATE: use four players in test
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/api/game/test_trick.py
+++ tests/whist/server/api/game/test_trick.py
@@ -7,17 +7,33 @@ class TrickTestCase(BaseCreateGameTestCase):
     def setUp(self):
         super().setUp()
         self.second_player = self.create_and_auth_user('miles', 'abc')
+        self.third_player = self.create_and_auth_user('nico', 'abc')
+        self.forth_player = self.create_and_auth_user('bot', 'abc')
 
         # Join the second player
         _ = self.client.post(url=f'/game/join/{self.game_id}',
                              json={'password': 'abc'},
                              headers=self.second_player)
+        # Join the third player
+        _ = self.client.post(url=f'/game/join/{self.game_id}',
+                             json={'password': 'abc'},
+                             headers=self.third_player)
+        # Join the forth player
+        _ = self.client.post(url=f'/game/join/{self.game_id}',
+                             json={'password': 'abc'},
+                             headers=self.forth_player)
         # Mark the first player ready
         _ = self.client.post(url=f'/game/action/ready/{self.game_id}',
                              headers=self.headers)
         # Mark the second player ready
         _ = self.client.post(url=f'/game/action/ready/{self.game_id}',
                              headers=self.second_player)
+        # Mark the third player ready
+        _ = self.client.post(url=f'/game/action/ready/{self.game_id}',
+                             headers=self.third_player)
+        # Mark the forth player ready
+        _ = self.client.post(url=f'/game/action/ready/{self.game_id}',
+                             headers=self.forth_player)
         # Request to start table.
         _ = self.client.post(url=f'/game/action/start/{self.game_id}',
                              headers=self.headers,

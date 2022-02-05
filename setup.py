97681a9cdb7526d6386ffe81c99a72910c159817
Marcel <25705862+Segelzwerg@users.noreply.github.com>
2022-02-05 14:36:16 +0100

    BUMP: server version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements/requirements.txt') as f:
 
 setup(
     name='whist-server',
-    version='0.1.0',
+    version='0.1.1',
     author='Whist Team',
     description='Whist game server.',
     long_description=long_description,

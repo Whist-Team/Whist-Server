9cee3576c604038db019e7784ec5106649a6d10d
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-02-06 13:36:17 +0100

    ADD: mocks for game service
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/api/game/base_token_case.py
+++ tests/whist/server/api/game/base_token_case.py
@@ -1,14 +1,20 @@
 import unittest
+from unittest.mock import MagicMock
 
 from starlette.testclient import TestClient
 
 from whist.server import app
 from whist.server.database import db
+from whist.server.services.game_db_service import GameDatabaseService
 
 
 class TestCaseWithToken(unittest.TestCase):
     def setUp(self) -> None:
+        self.game_service_mock = MagicMock(save=MagicMock())
+        app.dependency_overrides[GameDatabaseService] = lambda: self.game_service_mock
+
         self.client = TestClient(app)
+        self.app = app
         self.headers = self.create_and_auth_user('marcel', 'abc')
 
     def create_and_auth_user(self, user: str, password):

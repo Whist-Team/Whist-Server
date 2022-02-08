b80d5d0651b0b49c1bb99d88c4dbaeeb3b5b27b1
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-02-08 22:37:39 +0100

    UPDATE: use dependency injection for password service
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist/server/api/game/create.py
+++ whist/server/api/game/create.py
@@ -14,22 +14,22 @@ router = APIRouter(prefix='/game')
 
 @router.post('/create', status_code=200)
 def create_game(request: Dict[str, str], user: Player = Depends(get_current_user),
-                game_service=Depends(GameDatabaseService)):
+                game_service=Depends(GameDatabaseService), pwd_service=Depends(PasswordService)):
     """
     Creates a new game of whist.
     :param request: Must contain a 'game_name'. 'password' is optional.
     :param user: that created the game session.
     :param game_service: service to handle database interaction for games.
+    :param pwd_service: service to handle password requests.
     :return: the ID of the game instance.
     """
-    game_parameter = _set_game_parameter(request, user)
+    game_parameter = _set_game_parameter(request, user, pwd_service)
     game = game_service.create_with_pwd(**game_parameter)
     game_id = game_service.add(game)
     return {'game_id': game_id}
 
 
-def _set_game_parameter(request, user):
-    pwd_service = PasswordService()
+def _set_game_parameter(request, user, pwd_service: PasswordService):
     pwd_hash = _get_password(pwd_service, request)
     game_name = _get_game_name(request)
     game_parameter = dict(game_name=game_name,

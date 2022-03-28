caca182a651cd26a320296263b8c74d1523d8373
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-03-28 21:24:20 +0200

    ADD: test for not found
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/api/game/test_info.py
+++ tests/whist/server/api/game/test_info.py
@@ -7,3 +7,8 @@ class GameInfoTestCase(BaseCreateGameTestCase):
         response = self.client.get(f'/game/info/id/{game_name}')
         self.assertEqual(200, response.status_code, msg=response.content)
         self.assertEqual('1', response.json()['id'])
+
+    def test_game_name_to_id_not_found(self):
+        game_name: str = 'hornblower'
+        response = self.client.get(f'/game/info/id/{game_name}')
+        self.assertEqual(400, response.status_code, msg=response.content)

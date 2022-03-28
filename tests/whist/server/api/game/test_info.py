63e68b97a250947500f6683ef36b95d6ff72f328
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-03-28 22:10:20 +0200

    FIX: add mock for getter
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/api/game/test_info.py
+++ tests/whist/server/api/game/test_info.py
@@ -1,14 +1,19 @@
+from unittest.mock import MagicMock
+
 from tests.whist.server.api.game.base_created_case import BaseCreateGameTestCase
+from whist.server.services.error import GameNotFoundError
 
 
 class GameInfoTestCase(BaseCreateGameTestCase):
     def test_game_name_to_id(self):
+        self.game_service_mock.get_by_name = MagicMock(return_value=self.game_mock)
         game_name: str = 'albatros'
         response = self.client.get(f'/game/info/id/{game_name}')
         self.assertEqual(200, response.status_code, msg=response.content)
         self.assertEqual('1', response.json()['id'])
 
     def test_game_name_to_id_not_found(self):
+        self.game_service_mock.get_by_name = MagicMock(side_effect=GameNotFoundError())
         game_name: str = 'hornblower'
         response = self.client.get(f'/game/info/id/{game_name}')
         self.assertEqual(400, response.status_code, msg=response.content)

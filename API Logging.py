6b5368dbc1e41547be0b9b269b05d7d0bc6f72cc
abant07 <abantwal2007@gmail.com>
2022-04-09 07:53:32 -0700

    Update API Logging.py

--- API Logging.py
+++ API Logging.py
@@ -10,30 +10,28 @@ logger.addHandler(stream_handler)
 logger.setLevel(logging.INFO)
 
 #function that will log localhost, and available port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-target = "localhost"
-target_ip = socket.gethostbyname(target)
- 
-available_port = False
-port_count = 0
-
 def port_scan(port):
     try:
         s.connect((target_ip, port))
-        available_port = True
+        return available_port == True
     except:
-        available_port = False
+        return available_port == False
 
 #function that will log username
 def log_user(route):
     tokens = route.split("/")
-    logger.info(" " + tokens[2] + " has been created")
+    return " " + tokens[2] + " has been created"
 
-    
-    
+
 # call functions
 user_route = str(input("Enter a route: "))
-log_user(user_route)
+user_name = log_user(user_route)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+target = "localhost"
+target_ip = socket.gethostbyname(target)
+available_port = False
+port_count = 0
 
 while available_port == False:
     port_scan(port_count)
@@ -42,4 +40,5 @@ while available_port == False:
     else:
         port_count += 1
 
-logger.info(" " + target_ip + str(port_count) + " - " + user_route)
+logger.info(user_name)
+logger.info(" " + target_ip + ":" + str(port_count) + " - " + user_route)

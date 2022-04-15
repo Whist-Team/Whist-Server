8e20b43fca43c68732505e34119ce5bf40ea44ae
abant07 <100332169+abant07@users.noreply.github.com>
2022-04-15 13:27:09 -0700

    Update join.py

--- whist/server/api/game/join.py
+++ whist/server/api/game/join.py
@@ -11,16 +11,6 @@ from whist.server.services.authentication import get_current_user
 from whist.server.services.game_db_service import GameDatabaseService
 from whist.server.services.password import PasswordService
 
-import logging
-import sys
-
-logger = logging.getLogger(__name__)
-stream_handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter("%(levelname)s:%(message)s")
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
-logger.setLevel(logging.INFO)
-
 router = APIRouter(prefix='/game')
 
 

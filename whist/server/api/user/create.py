332a066b28d88d8b882e29a1157a9e3f33a644fc
abant07 <100332169+abant07@users.noreply.github.com>
2022-04-15 10:37:47 -0700

    Update create.py

--- whist/server/api/user/create.py
+++ whist/server/api/user/create.py
@@ -43,5 +43,5 @@ def create_user(request: Dict[str, str]):
                     hashed_password=pwd_hash)
     user_db_service = UserDatabaseService()
     user_id = user_db_service.add(user)
-    #return {'user_id': user_id}
-    return logger.info(user.username + " has been created")
+    logger.info(user.username + " has been created")
+    return {'user_id': user_id}

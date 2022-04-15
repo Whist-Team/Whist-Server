b070c1a567a7d70f470c66eee5f0dee95b323ce5
abant07 <100332169+abant07@users.noreply.github.com>
2022-04-15 13:32:13 -0700

    Update create.py

--- whist/server/api/user/create.py
+++ whist/server/api/user/create.py
@@ -7,16 +7,6 @@ from whist.server.database.user import UserInDb
 from whist.server.services.password import PasswordService
 from whist.server.services.user_db_service import UserDatabaseService
 
-import logging
-import sys
-
-logger = logging.getLogger(__name__)
-stream_handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter("%(levelname)s:%(message)s")
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
-logger.setLevel(logging.INFO)
-
 router = APIRouter(prefix='/user')
 
 

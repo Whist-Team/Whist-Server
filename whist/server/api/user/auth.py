e47719c532dc7d8c0e9b39234d097eeee1ee6186
abant07 <100332169+abant07@users.noreply.github.com>
2022-04-16 08:35:20 -0700

    Update auth.py

--- whist/server/api/user/auth.py
+++ whist/server/api/user/auth.py
@@ -39,5 +39,5 @@ async def auth(request: OAuth2PasswordRequestForm = Depends()):
         ) from error
     token_request = {'sub': username}
     token = create_access_token(token_request)
-    logger.info(username + " has logged in")
+    logger.info(f"{username} has logged in")
     return AccessToken(token=token, token_type='Bearer')  # nosec B106

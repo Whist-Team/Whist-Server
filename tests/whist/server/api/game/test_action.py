174a66b4bcbd266ffc2e72878d339cc5ea0eae9f
abant07 <100332169+abant07@users.noreply.github.com>
2022-05-27 08:46:50 -0700

    Update test_action.py

--- tests/whist/server/api/game/test_action.py
+++ tests/whist/server/api/game/test_action.py
@@ -7,6 +7,18 @@ from whist.server.database.error import PlayerNotCreatorError
 from whist.server.services.game_db_service import GameDatabaseService
 
 
+class PlayerNotReadyError(Exception):
+    """
+    Will check to see if player is ready before unreadying
+    """
+
+
+class GameNotFoundError(Exception):
+    """
+    Checks to see if user enters correct game_id
+    """
+
+
 class ActionGameTestCase(BaseCreateGameTestCase):
     def setUp(self) -> None:
         super().setUp()
@@ -53,4 +65,31 @@ class ActionGameTestCase(BaseCreateGameTestCase):
         response = self.client.post(url=f'/game/action/ready/{self.game_mock.id}',
                                     headers=self.second_player)
         self.game_mock.ready_player.assert_called_once()
+        self.assertEqual(403, response.status_code, msg=response.content)
+
+    def test_unready(self):
+        response = self.client.post(url=f'/game/action/unready/{self.game_mock.id}',
+                                    headers=self.headers)
+        self.game_mock.unready_player.assert_called_once()
+        self.assertEqual(200, response.status_code, msg=response.content)
+
+    def test_unready_game_not_found(self):
+        self.game_mock.unready_player = MagicMock(side_effect=GameNotFoundError)
+        response = self.client.post(url=f'/game/action/unready/{self.game_mock.id}',
+                                    headers=self.second_player)
+        self.game_mock.unready_player.assert_called_once()
+        self.assertEqual(403, response.status_code, msg=response.content)
+
+    def test_unready_not_joined(self):
+        self.game_mock.unready_player = MagicMock(side_effect=PlayerNotJoinedError)
+        response = self.client.post(url=f'/game/action/unready/{self.game_mock.id}',
+                                    headers=self.second_player)
+        self.game_mock.unready_player.assert_called_once()
+        self.assertEqual(403, response.status_code, msg=response.content)
+
+    def test_unready_player_not_ready(self):
+        self.game_mock.unready_player = MagicMock(side_effect=PlayerNotReadyError)
+        response = self.client.post(url=f'/game/action/unready/{self.game_mock.id}',
+                                    headers=self.second_player)
+        self.game_mock.unready_player.assert_called_once()
         self.assertEqual(403, response.status_code, msg=response.content)

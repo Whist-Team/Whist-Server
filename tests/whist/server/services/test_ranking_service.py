49d232bdf09e4d318976a0917d907aa5950ef04a
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-07 21:43:08 +0200

    ADD: test for ranking service
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

---
+++ tests/whist/server/services/test_ranking_service.py
@@ -0,0 +1,20 @@
+from tests.whist.server.services.base_user_test_case import UserBaseTestCase
+from whist.server.const import INITIAL_RATING
+
+from whist.server.database.user import UserInDb
+from whist.server.services.ranking_service import RankingService
+from whist.server.services.user_db_service import UserDatabaseService
+
+
+class LeaderboardTestCase(UserBaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.second_user = UserInDb(username='lower_ranking', rating=INITIAL_RATING - 10)
+        self.ranking_service = RankingService()
+        user_service = UserDatabaseService()
+        user_service.add(self.user)
+        user_service.add(self.second_user)
+
+    def test_correct_order(self):
+        ranking = self.ranking_service.all()
+        self.assertEqual([self.user.to_user(), self.second_user.to_user()], ranking)

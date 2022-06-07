d17864c59f2fd67cb3e5cf9e15bcf66196c0c650
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-07 21:42:52 +0200

    REFACTOR: extract base test case
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

---
+++ tests/whist/server/services/base_user_test_case.py
@@ -0,0 +1,13 @@
+from whist.server.database import db
+from whist.server.database.user import UserInDb
+from whist.server.services.user_db_service import UserDatabaseService
+
+
+class UserBaseTestCase:
+    def setUp(self):
+        self.user_database_service = UserDatabaseService()
+        self.user: UserInDb = UserInDb(username='test', hashed_password='abc')
+        db.user.drop()
+
+    def tearDown(self) -> None:
+        db.user.drop()

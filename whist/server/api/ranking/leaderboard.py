c7611591a2149ecf3cc576bfa042d97d8e0b67f7
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-07 23:10:38 +0200

    FIX: injection of ranking service
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist/server/api/ranking/leaderboard.py
+++ whist/server/api/ranking/leaderboard.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from whist.core.user.player import Player
 
 from whist.server.services.ranking_service import RankingService
@@ -7,12 +7,12 @@ router = APIRouter(prefix='/leaderboard')
 
 
 @router.get('/{order}', response_model=list[Player])
-def get_ranking_by(order: str) -> list[Player]:
+def get_ranking_by(order: str, ranking_service=Depends(RankingService)) -> list[Player]:
     """
     Retrieves a ranking of the players by selected order.
     :param order: either 'ascending' or 'descending'
+    :param ranking_service: Dependency injection of ranking service.
     :return: sorted list of players in chosen order
     """
-    ranking_service = RankingService()
     leaderboard = ranking_service.all(order)
     return leaderboard

f0c8c4b70fc20f33ec753621f455b7186639e9a0
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-12 21:53:32 +0200

    ADD: subscriber
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

---
+++ whist/server/api/publish/subscriber.py
@@ -0,0 +1,25 @@
+"""Client abstraction"""
+from fastapi import WebSocket
+
+from whist.server.api.publish.events.event import Event
+
+
+class Subscriber:
+    """
+    A subscriber represents one client.
+    """
+
+    def __init__(self, connection: WebSocket):
+        """
+        Constructor
+        :param connection: Implementation of the web socket connection.
+        """
+        self._connection = connection
+
+    def send(self, event: Event) -> None:
+        """
+        Sends one event to this client.
+        :param event: Any type of event.
+        :return: None
+        """
+        self._connection.send_json(event)

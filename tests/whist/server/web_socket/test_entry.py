f6609142ff267139dbcaeef7823b44d07358a300
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-13 22:35:36 +0200

    FIX: test
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/web_socket/test_entry.py
+++ tests/whist/server/web_socket/test_entry.py
@@ -1,12 +1,4 @@
-import asyncio
-
 from tests.whist.server.base_token_case import TestCaseWithToken
-
-
-async def print_response(websocket):
-    print('wait for response')
-    response = await websocket.receive_text
-    print(response)
 
 
 class EntryTestCase(TestCaseWithToken):
@@ -26,5 +18,5 @@ class EntryTestCase(TestCaseWithToken):
         with self.client.websocket_connect('/room/{self.room_id}') as websocket:
             data = {'token': self.token}
             websocket.send_json(data)
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(print_response(websocket))
+            response = websocket.receive_text()
+            self.assertEqual('200', response)

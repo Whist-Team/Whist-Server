f70d2706f701cb9fbbe936a5ca44f014230873e4
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-13 22:55:45 +0200

    REFACTOR: mock central setup
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/web_socket/test_entry.py
+++ tests/whist/server/web_socket/test_entry.py
@@ -13,6 +13,8 @@ class EntryTestCase(TestCaseWithToken):
         data = {'game_name': 'test', 'password': 'abc'}
         response = self.client.post(url='/game/create', json=data, headers=self.headers)
         self.room_id = response.json()['game_id']
+        channel_mock = MagicMock()
+        self.game_mock = MagicMock(side_channel=channel_mock)
 
     def test_ping(self):
         with self.client.websocket_connect('/ping') as websocket:
@@ -20,25 +22,21 @@ class EntryTestCase(TestCaseWithToken):
             self.assertEqual('pong', text)
 
     def test_subscribe(self):
-        channel_mock = MagicMock()
-        game_mock = MagicMock(side_channel=channel_mock)
-        self.game_service_mock.get = MagicMock(return_value=game_mock)
+        self.game_service_mock.get = MagicMock(return_value=self.game_mock)
         app.dependency_overrides[GameDatabaseService] = lambda: self.game_service_mock
         with self.client.websocket_connect(f'/room/{self.room_id}') as websocket:
             data = {'token': self.token}
             websocket.send_json(data)
             response = websocket.receive_text()
-            game_mock.side_channel.attach.assert_called_once()
+            self.game_mock.side_channel.attach.assert_called_once()
             self.assertEqual('200', response)
 
     def test_subscribe_room_not_exists(self):
-        channel_mock = MagicMock()
-        game_mock = MagicMock(side_channel=channel_mock)
         self.game_service_mock.get = MagicMock(side_effect=GameNotFoundError)
         app.dependency_overrides[GameDatabaseService] = lambda: self.game_service_mock
         with self.client.websocket_connect(f'/room/{self.room_id}') as websocket:
             data = {'token': self.token}
             websocket.send_json(data)
             response = websocket.receive_text()
-            game_mock.side_channel.attach.assert_not_called()
+            self.game_mock.side_channel.attach.assert_not_called()
             self.assertEqual('Game not found', response)

427b945d36294182414044c4787dd5e56fc57830
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-14 18:18:49 +0200

    FIX: dependency injection user db service
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist/server/web_socket/entry.py
+++ whist/server/web_socket/entry.py
@@ -5,6 +5,7 @@ from whist.core.error.table_error import PlayerNotJoinedError
 from whist.server.services.authentication import get_current_user
 from whist.server.services.error import GameNotFoundError
 from whist.server.services.game_db_service import GameDatabaseService
+from whist.server.services.user_db_service import UserDatabaseService
 from whist.server.web_socket.subscriber import Subscriber
 
 router = APIRouter()
@@ -22,18 +23,20 @@ async def ping(websocket: WebSocket):
 
 @router.websocket('/room/{room_id}')
 async def subscribe_room(websocket: WebSocket, room_id: str,
-                         game_service: GameDatabaseService = Depends(GameDatabaseService)):
+                         game_service: GameDatabaseService = Depends(GameDatabaseService),
+                         user_service: UserDatabaseService = Depends(UserDatabaseService)):
     """
     Clients requests to subscribe to room's side channel.
     :param websocket: communication end point of the client
     :param room_id: ID of the room to which should be subscribed
     :param game_service: handles all request to the db regarding rooms.
+    :param user_service: handles all request to the db regarding users.
     :return: Sends response to the websockets. No return.
     """
     await websocket.accept()
     try:
         token = await websocket.receive_json()
-        player = await get_current_user(token['token'])
+        player = await get_current_user(token['token'], user_service)
         subscriber = Subscriber(websocket)
         room = game_service.get(room_id)
         _ = room.get_player(player)

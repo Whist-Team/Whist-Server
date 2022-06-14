f79a06d55905d0760e9395d0c5c12dce36035da8
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-14 22:47:41 +0200

    FIX: variable usage
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/web_socket/test_event.py
+++ tests/whist/server/web_socket/test_event.py
@@ -1,14 +1,13 @@
 from unittest import TestCase
-from unittest.mock import MagicMock
 
+from whist.core.user.player import Player
 from whist.server.web_socket.events.event import PlayerJoined
 
 
 class EventTestCase(TestCase):
     def test_player_joined(self):
-        player = MagicMock()
+        player = Player(username='ititus', rating=100)
         expected_dict = {'player': player}
 
-        event = PlayerJoined(player)
-
+        event = PlayerJoined(player=player)
         self.assertEqual(expected_dict, dict(event))
--- whist/server/web_socket/events/event.py
+++ whist/server/web_socket/events/event.py
@@ -2,9 +2,17 @@
 from abc import ABC
 
 from pydantic import BaseModel
+from whist.core.user.player import Player
 
 
 class Event(ABC, BaseModel):
     """
     It is sent via the websocket upon Game State changes.
     """
+
+
+class PlayerJoined(Event):
+    """
+    It is sent when a player joins a game.
+    """
+    player: Player

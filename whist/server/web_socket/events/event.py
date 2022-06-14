2a01b30e341b355e765146d0dc4e1500f347a21c
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-14 23:26:18 +0200

    ADD: more events
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist/server/web_socket/events/event.py
+++ whist/server/web_socket/events/event.py
@@ -2,6 +2,7 @@
 from abc import ABC
 
 from pydantic import BaseModel
+from whist.core.cards.card import Card
 from whist.core.user.player import Player
 
 
@@ -18,8 +19,22 @@ class Event(ABC, BaseModel):
         return self.__class__.__name__
 
 
+class CardPlayedEvent(Event):
+    """
+    It is sent when a player plays card.
+    """
+    card: Card
+    player: Player
+
+
 class PlayerJoinedEvent(Event):
     """
     It is sent when a player joins a game.
     """
     player: Player
+
+
+class RoomStartedEvent(Event):
+    """
+    It is sent when a room has been started.
+    """

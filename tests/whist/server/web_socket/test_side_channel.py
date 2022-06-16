ee044937e9bc9fff7de97a4af625d8e1808319c4
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-16 11:20:01 +0200

    FIX: tests
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/web_socket/test_side_channel.py
+++ tests/whist/server/web_socket/test_side_channel.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from unittest.mock import MagicMock
 
 from whist.core.user.player import Player
+
 from whist.server.web_socket.events.event import PlayerJoinedEvent
 from whist.server.web_socket.side_channel import SideChannel
 from whist.server.web_socket.subscriber import Subscriber
@@ -18,7 +19,8 @@ class TestCase(TestCase):
     def test_send_joined(self):
         self.side_channel.attach(self.subscriber)
         self.side_channel.notify(self.event)
-        self.connection_mock.send_json.assert_called_with({'PlayerJoinedEvent': self.event})
+        self.connection_mock.send_json.assert_called_with({'name': 'PlayerJoinedEvent',
+                                                           'event': self.event})
 
     def test_send_not_joined(self):
         self.side_channel.notify(self.event)

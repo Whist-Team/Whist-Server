7a034ad8afd6aed9594ca3ccba1002783a373a0f
iTitus <iTitus@users.noreply.github.com>
2022-06-18 19:08:34 +0200

    remove database dropping

--- whist/server/database/connection.py
+++ whist/server/database/connection.py
@@ -1,14 +1,10 @@
 """Connection to mongodb instance"""
 import pymongo
 
-from whist.server.const import TEST_DATABASE
-
 
 def get_database(database):
     """
     Returns the mongo database.
     """
     client = pymongo.MongoClient(host=database)
-    if database == TEST_DATABASE:
-        client.drop_database(database)
     return client[database]

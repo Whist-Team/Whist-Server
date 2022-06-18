f70741cf55d98ec289a4ccb784a57e061dd03bb4
iTitus <iTitus@users.noreply.github.com>
2022-06-18 18:50:57 +0200

    Fix typo and wrong assumption in CLI code

--- whist/server/__main__.py
+++ whist/server/__main__.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser.add_argument('--admin_name', type=str, help="Admin's username")
     parser.add_argument('--admin_pwd', type=str,
                         help="Admin's inital password. Must be changed after deployment.")
-    parser.add_argument('--reload', help='Enable debug reloading for the uvicorn server')
+    parser.add_argument('--reload', action='store_true', help='Enable debug reloading for the uvicorn server')
     args = parser.parse_args()
     main(host=args.host_addr, port=args.host_port, admin_name=args.admin_name,
-         admin_pwd=args.admin_pwd, reload=parser.reload)
+         admin_pwd=args.admin_pwd, reload=args.reload)
--- whist/server/cli.py
+++ whist/server/cli.py
@@ -1,6 +1,5 @@
 """CLI entrypoint"""
 import uvicorn
-from whist.server import app
 from whist.server.const import HOST_ADDR, HOST_PORT, ADMIN_NAME, ADMIN_PASSWORD
 from whist.server.database.user import UserInDb
 from whist.server.services.password import PasswordService
@@ -15,4 +14,4 @@ def main(host=HOST_ADDR, port=HOST_PORT, admin_name=ADMIN_NAME, admin_pwd=ADMIN_
         password_service = PasswordService()
         admin = UserInDb(username=admin_name, hashed_password=password_service.hash(admin_pwd))
         user_service.add(admin)
-    uvicorn.run(app, host=host, port=port, reload=reload is not None and reload)
+    uvicorn.run('whist.server:app', host=host, port=port, reload=reload is not None and reload)

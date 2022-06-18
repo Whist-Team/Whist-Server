fd1ec9e7be690a0422ebfbcbea0879bd9d564dc4
iTitus <iTitus@users.noreply.github.com>
2022-06-18 15:12:16 +0200

    fix lint

--- whist/server/cli.py
+++ whist/server/cli.py
@@ -7,7 +7,8 @@ from whist.server.services.password import PasswordService
 from whist.server.services.user_db_service import UserDatabaseService
 
 
-def main(host=HOST_ADDR, port=HOST_PORT, admin_name=ADMIN_NAME, admin_pwd=ADMIN_PASSWORD, reload=False):
+def main(host=HOST_ADDR, port=HOST_PORT, admin_name=ADMIN_NAME, admin_pwd=ADMIN_PASSWORD,
+         reload=False):
     """Main function."""
     if admin_name is not None and admin_pwd is not None:
         user_service = UserDatabaseService()

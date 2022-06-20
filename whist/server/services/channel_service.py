50ec95add1ce6c2f71898b0d6a58e1e4b31fa8a6
Marcel <25705862+Segelzwerg@users.noreply.github.com>
2022-06-20 19:12:42 +0200

    ADD: documentation

--- whist/server/services/channel_service.py
+++ whist/server/services/channel_service.py
@@ -1,3 +1,4 @@
+"""Side Channel Manager"""
 from whist.server.services.error import ChannelAlreadyExistsError, ChannelNotFoundError
 from whist.server.web_socket.events.event import Event
 from whist.server.web_socket.side_channel import SideChannel
@@ -5,6 +6,9 @@ from whist.server.web_socket.subscriber import Subscriber
 
 
 class ChannelService:
+    """
+    Manages websocket side-channel clients
+    """
     _instance = None
     _channels: dict[str, SideChannel] = None
 
@@ -16,12 +20,24 @@ class ChannelService:
 
     @classmethod
     def add(cls, room_id: str, channel: SideChannel) -> None:
+        """
+        Adds a side-channel to the service.
+        :param room_id: ID of the room the side-channel is associated with
+        :param channel: the side-channel wrapper
+        :return: None
+        """
         if room_id in cls._channels.keys():
             raise ChannelAlreadyExistsError()
         cls._channels.update({room_id: channel})
 
     @classmethod
     def attach(cls, room_id: str, subscriber: Subscriber) -> None:
+        """
+        Adds a client to a side-channel.
+        :param room_id: ID of the room the side-channel is associated with
+        :param channel: the client wrapper
+        :return: None
+        """
         if room_id not in cls._channels.keys():
             side_channel = SideChannel()
             cls.add(room_id, side_channel)
@@ -29,12 +45,23 @@ class ChannelService:
 
     @classmethod
     def remove(cls, room_id: str) -> None:
+        """
+        Removes a side-channel from the service.
+        :param room_id: ID of the room the side-channel is associated with
+        :return: None
+        """
         if room_id not in cls._channels.keys():
             raise ChannelNotFoundError()
         cls._channels.pop(room_id)
 
     @classmethod
     def notify(cls, room_id: str, event: Event) -> None:
+        """
+        Multicast to all clients of a room.
+        :param room_id: ID of the room the side-channel is associated with
+        :param event: the wrapped information of what to broadcast
+        :return: None
+        """
         if room_id not in cls._channels.keys():
             raise ChannelNotFoundError()
         cls._channels.get(room_id).notify(event)

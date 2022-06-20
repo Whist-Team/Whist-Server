2cbd80326ee78186ae9e599a3c8e486f27cef0a9
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-20 22:35:14 +0200

    UPDATE: require login for leaderboard
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist/server/api/ranking/leaderboard.py
+++ whist/server/api/ranking/leaderboard.py
@@ -2,16 +2,19 @@
 from fastapi import APIRouter, Depends
 from whist.core.user.player import Player
 
+from whist.server.services.authentication import get_current_user
 from whist.server.services.ranking_service import RankingService
 
 router = APIRouter(prefix='/leaderboard')
 
 
 @router.get('/{order}', response_model=list[Player])
-def get_ranking_by(order: str, ranking_service=Depends(RankingService)) -> list[Player]:
+def get_ranking_by(order: str, user: Player = Depends(get_current_user),
+                   ranking_service=Depends(RankingService)) -> list[Player]:
     """
     Retrieves a ranking of the players by selected order.
     :param order: either 'ascending' or 'descending'
+    :param user: not required for login, but authentication
     :param ranking_service: Dependency injection of ranking service.
     :return: sorted list of players in chosen order
     """

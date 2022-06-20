d6396132b3cb3388d6bacbaa17464990a26a5e71
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-20 23:04:19 +0200

    FIX: not joined test
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/api/game/test_info.py
+++ tests/whist/server/api/game/test_info.py
@@ -20,11 +20,11 @@ class GameInfoTestCase(BaseCreateGameTestCase):
         self.assertEqual(401, response.status_code, msg=response.content)
 
     def test_game_name_to_id_not_joined(self):
-        self.app.dependency_overrides = {}
+        self.game_mock.has_joined = MagicMock(return_value=False)
         self.game_service_mock.get_by_name = MagicMock(return_value=self.game_mock)
         game_name: str = 'albatros'
         response = self.client.get(f'/game/info/id/{game_name}')
-        self.assertEqual(401, response.status_code, msg=response.content)
+        self.assertEqual(403, response.status_code, msg=response.content)
 
     def test_game_name_to_id_not_found(self):
         self.game_service_mock.get_by_name = MagicMock(side_effect=GameNotFoundError())

0fa0e3872bc4031a178a44309d2fe0c11707bd37
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-21 22:20:49 +0200

    REMOVE: no longer needed id
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist/server/database/game_info.py
+++ whist/server/database/game_info.py
@@ -1,9 +1,6 @@
 """DAO of game info"""
-from typing import Optional
 
-from pydantic import BaseModel, Field
-
-from whist.server.database.id_wrapper import PyObjectId
+from pydantic import BaseModel
 
 
 # DAO
@@ -12,6 +9,5 @@ class GameInfo(BaseModel):
     """
     Contains meta data of the server.
     """
-    id: Optional[PyObjectId] = Field(alias='_id')
     game: str
     version: str

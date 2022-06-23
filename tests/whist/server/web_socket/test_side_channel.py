bbe2b41d4f41af43e43bf51b73207c2740699b16
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-23 17:51:28 +0200

    FIX: mocking
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/web_socket/test_side_channel.py
+++ tests/whist/server/web_socket/test_side_channel.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
 from whist.core.user.player import Player
 
@@ -8,7 +8,6 @@ from whist.server.web_socket.side_channel import SideChannel
 from whist.server.web_socket.subscriber import Subscriber
 
 
-@patch('asyncio.run')
 class TestCase(TestCase):
     def setUp(self):
         self.connection_mock = MagicMock()
@@ -17,18 +16,18 @@ class TestCase(TestCase):
         self.event = PlayerJoinedEvent(player=player)
         self.side_channel = SideChannel()
 
-    def test_send_joined(self, run_mock):
+    def test_send_joined(self):
         self.side_channel.attach(self.subscriber)
         self.side_channel.notify(self.event)
-        run_mock.assert_called_with(self.connection_mock.send_json(
-            {'name': self.event.name, 'event': self.event.json()}))
+        self.connection_mock.send_json.assert_called_with({'name': self.event.name, 'event':
+            self.event.json()})
 
-    def test_send_not_joined(self, run_mock):
+    def test_send_not_joined(self):
         self.side_channel.notify(self.event)
-        run_mock.send_json.assert_not_called()
+        self.connection_mock.send_json.assert_not_called()
 
-    def test_send_left(self, run_mock):
+    def test_send_left(self):
         self.side_channel.attach(self.subscriber)
         self.side_channel.remove(self.subscriber)
         self.side_channel.notify(self.event)
-        run_mock.send_json.assert_not_called()
+        self.connection_mock.send_json.assert_not_called()
--- tests/whist/server/web_socket/test_subscriber.py
+++ tests/whist/server/web_socket/test_subscriber.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
 from whist.server.web_socket.events.event import Event
 from whist.server.web_socket.subscriber import Subscriber
@@ -10,9 +10,8 @@ class SubscriberTestCase(TestCase):
         self.connection_mock = MagicMock()
         self.subscriber = Subscriber(self.connection_mock)
 
-    @patch('asyncio.run')
-    def test_send(self, run_mock):
+    def test_send(self):
         event = Event()
         self.subscriber.send(event)
-        run_mock.assert_called_with(
-            self.connection_mock.send_json({'name': event.name, 'event': event.json()}))
+        self.connection_mock.send_json.assert_called_with({'name': event.name, 'event':
+            event.json()})

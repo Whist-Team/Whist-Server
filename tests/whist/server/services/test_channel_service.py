8c36328ae0c130746cad43e660fa8a450eb6613c
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-25 23:04:58 +0200

    FIX: async mocks
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist/server/services/test_channel_service.py
+++ tests/whist/server/services/test_channel_service.py
@@ -1,5 +1,6 @@
+import asyncio
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, AsyncMock
 
 from whist.server.services.channel_service import ChannelService
 from whist.server.services.error import ChannelNotFoundError, ChannelAlreadyExistsError
@@ -8,13 +9,13 @@ from whist.server.web_socket.events.event import Event
 
 class ChannelServiceTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.channel = MagicMock()
+        self.channel = MagicMock(notify=AsyncMock())
         self.service = ChannelService()
 
     def test_notify(self):
         self.service.add('1', self.channel)
         event = Event()
-        self.service.notify('1', event)
+        asyncio.run(self.service.notify('1', event))
         self.channel.notify.assert_called_once_with(event)
 
     def test_notify_removed(self):
@@ -22,12 +23,12 @@ class ChannelServiceTestCase(unittest.TestCase):
         self.service.remove('2')
         event = Event()
         with self.assertRaises(ChannelNotFoundError):
-            self.service.notify('2', event)
+            asyncio.run(self.service.notify('2', event))
 
     def test_notify_not_added(self):
         event = Event()
         with self.assertRaises(ChannelNotFoundError):
-            self.service.notify('3', event)
+            asyncio.run(self.service.notify('3', event))
 
     def test_remove_not_added(self):
         with self.assertRaises(ChannelNotFoundError):

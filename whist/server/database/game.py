0cda93e9b02aac34857c715a8f68799981e1af14
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-27 22:12:57 +0200

    FIX: start game manually FIX: use current game as getter
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist/server/database/game.py
+++ whist/server/database/game.py
@@ -118,8 +118,9 @@ class Game(BaseModel):
             raise PlayerNotCreatorError()
         if not self.table.started:
             self.table.start(matcher)
-        self.current_trick(auto_next=True)
-        return self.table.started
+        started = self.table.started
+        self.table.current_rubber.next_game()
+        return started
 
     def get_player(self, player) -> PlayerAtTable:
         """
@@ -133,7 +134,7 @@ class Game(BaseModel):
         return self._current_game().next_hand()
 
     def _current_game(self):
-        return self.current_rubber.next_game()
+        return self.current_rubber.current_game()
 
 
 class GameInDb(Game):

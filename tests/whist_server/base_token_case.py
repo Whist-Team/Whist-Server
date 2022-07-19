bc5f7a269fdd9ef7b2806ec5fed71a8bca4e2328
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-07-19 23:38:51 +0200

    UPDATE: use mocks instead of db calls
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist_server/base_token_case.py
+++ tests/whist_server/base_token_case.py
@@ -5,7 +5,6 @@ from starlette.testclient import TestClient
 from whist_core.user.player import Player
 
 from whist_server import app
-from whist_server.database import db
 from whist_server.services.authentication import get_current_user
 from whist_server.services.channel_service import ChannelService
 from whist_server.services.password import PasswordService
@@ -15,8 +14,6 @@ from whist_server.services.user_db_service import UserDatabaseService
 
 class TestCaseWithToken(unittest.TestCase):
     def setUp(self) -> None:
-        db.room.drop()
-        db.user.drop()
         self.player_mock = Player(username='marcel', rating=2000)
         user_mock = MagicMock(name='user', to_user=self.player_mock)
         user_service = MagicMock(get=MagicMock(return_value=user_mock))
@@ -31,14 +28,8 @@ class TestCaseWithToken(unittest.TestCase):
 
         self.client = TestClient(app)
         self.app = app
-        self.headers = self.create_and_auth_user('marcel', 'abc')
+        self.headers = self.create_and_auth_user()
 
-    def create_and_auth_user(self, user: str, password):
-        login_creds = {'username': user, 'password': password}
-        _ = self.client.post(url='/user/create', json=login_creds)
-        token = self.client.post(url='/user/auth', data=login_creds).json()['access_token']
+    def create_and_auth_user(self):
+        token = '1' * 24
         return {'Authorization': f'Bearer {token}'}
-
-    def tearDown(self) -> None:
-        db.room.drop()
-        db.user.drop()

56d4af9bcaa22d73e0f3e3e1155c3bfe919220e2
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-08-30 22:53:59 +0200

    ADD: test for failure
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist_server/api/game/test_game.py
+++ tests/whist_server/api/game/test_game.py
@@ -1,5 +1,7 @@
 from unittest.mock import MagicMock
 
+from whist_core.game.errors import HandNotDoneError
+
 from tests.whist_server.api.game.base_created_case import BaseCreateGameTestCase
 
 
@@ -12,3 +14,10 @@ class GameTestCase(BaseCreateGameTestCase):
     def test_next_hand_success(self):
         response = self.client.post(url=f'/room/next_hand/{self.room_mock.id}')
         self.assertEqual('Success', response.json()['status'])
+        self.assertEqual(200, response.status_code)
+
+    def test_next_hand_not_done(self):
+        self.room_mock.next_hand = MagicMock(side_effect=HandNotDoneError())
+        response = self.client.post(url=f'/room/next_hand/{self.room_mock.id}')
+        self.assertEqual('Failed', response.json()['status'])
+        self.assertEqual(400, response.status_code)

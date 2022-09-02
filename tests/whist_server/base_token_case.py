6456de9f3eac00019fc9602305798d7b9504364f
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-09-02 22:36:38 +0200

    FIX: splunk service mocking
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist_server/base_token_case.py
+++ tests/whist_server/base_token_case.py
@@ -9,6 +9,7 @@ from whist_server.services.authentication import get_current_user
 from whist_server.services.channel_service import ChannelService
 from whist_server.services.password import PasswordService
 from whist_server.services.room_db_service import RoomDatabaseService
+from whist_server.services.splunk_service import SplunkService
 from whist_server.services.user_db_service import UserDatabaseService
 
 
@@ -21,11 +22,13 @@ class TestCaseWithToken(unittest.TestCase):
         self.password_service_mock = MagicMock(verify=MagicMock(),
                                                hash=MagicMock(return_value='1' * 12))
         self.channel_service_mock = MagicMock()
+        self.splunk_service_mock = MagicMock()
         app.dependency_overrides[ChannelService] = lambda: self.channel_service_mock
         app.dependency_overrides[RoomDatabaseService] = lambda: self.room_service_mock
         app.dependency_overrides[PasswordService] = lambda: self.password_service_mock
         app.dependency_overrides[UserDatabaseService] = lambda: self.user_service_mock
         app.dependency_overrides[get_current_user] = lambda: self.player_mock
+        app.dependency_overrides[SplunkService] = lambda: self.splunk_service_mock
 
         self.client = TestClient(app)
         self.app = app

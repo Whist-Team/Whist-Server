4d93645f741cb0c1a27bd7a125199fe85f2668cf
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-09-02 21:27:25 +0200

    ADD: writing splunk events
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist_server/services/splunk_service.py
+++ whist_server/services/splunk_service.py
@@ -1,5 +1,38 @@
 """Splunk Integration"""
+from typing import Optional
+
 from splunklib import client
+
+
+class SplunkEvent:
+    """
+    Wrapper for splunk events.
+    """
+    def __init__(self, event: str, source: Optional[str] = None, source_type: Optional[str] = None):
+        self._event = event
+        self._source = source
+        self._source_type = source_type
+
+    @property
+    def event(self):
+        """
+        Event message as string
+        """
+        return self._event
+
+    @property
+    def source(self):
+        """
+        The origin of the event.
+        """
+        return self._source
+
+    @property
+    def sourcetype(self):
+        """
+        The type of the event.
+        """
+        return self._source_type
 
 
 class SplunkService:
@@ -17,3 +50,13 @@ class SplunkService:
                 port=port,
                 splunkToken=token)
         return cls._instance
+
+    @classmethod
+    def write_event(cls, event: SplunkEvent) -> None:
+        """
+        Forwards an event to Splunk
+        :param event: to be forwarded
+        :return: None
+        """
+        index = cls._service.indexes['whist_monitor']
+        index.submit(event=event.event, source=event.source, sourcetype=None)

c3afab42c0e9f3d2732db1f864540230599caca5
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-09-02 21:53:27 +0200

    ADD: test for write event
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

---
+++ tests/whist_server/services/test_splunk_service.py
@@ -0,0 +1,18 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+from whist_server.services.splunk_service import SplunkEvent, SplunkService
+
+
+class SplunkServiceTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.index_mock = MagicMock()
+        self.service_mock = MagicMock(indexes={'whist_monitor': self.index_mock})
+
+    def test_write_event(self):
+        event = SplunkEvent(event='Start', source='Test', source_type='testing')
+        with patch('splunklib.client.connect', return_value=self.service_mock):
+            service = SplunkService('localhost', 7000, 'abc')
+            service.write_event(event)
+            self.index_mock.submit.assert_called_once_with(event=event.event, source=event.source,
+                                                           sourcetype=event.sourcetype)

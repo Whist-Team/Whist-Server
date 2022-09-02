f05f6f6c5e9ecf6279e80cd3f6f8af33bf824919
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-09-03 01:53:38 +0200

    FIX: env getter
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist_server/services/splunk_service.py
+++ whist_server/services/splunk_service.py
@@ -37,12 +37,6 @@ class SplunkEvent:
         return self._source_type
 
 
-class SplunkWarning(Warning):
-    """
-    Is raised when something is not working with Splunk.
-    """
-
-
 class SplunkService:
     """
     Service for Splunk Integration.
@@ -54,13 +48,13 @@ class SplunkService:
         if cls._instance is None:
             cls._instance = super(SplunkService, cls).__new__(cls)
 
-            host = os.getenv('SPLUNK_HOST')
-            port = os.getenv('SPLUNK_PORT')
-            token = os.getenv('SPLUNK_TOKEN')
-            if host is not None and port is not None and token is not None:
+            try:
+                host = os.environ['SPLUNK_HOST']
+                port = int(os.environ['SPLUNK_PORT'])
+                token = os.environ['SPLUNK_TOKEN']
                 cls._service = client.connect(host=host, port=port, splunkToken=token)
-            else:
-                raise SplunkWarning('No Splunk parameters setup.')
+            except KeyError:
+                print('Splunk parameter are not set.')
         return cls._instance
 
     @classmethod

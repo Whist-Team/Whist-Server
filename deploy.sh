ae67308455bb530c79a6fbcc0aaf6ae0422f2acf
Marcel <25705862+Segelzwerg@users.noreply.github.com>
2022-11-23 23:29:59 +0100

    FIX: spacing

--- deploy.sh
+++ deploy.sh
@@ -5,28 +5,28 @@ echo "Mongo server started"
 
 echo "Enter host address for whist server (127.0.0.1):"
 read host_addr
-if [ -z "$host_addr"]
+if [ -z "$host_addr" ]
 then
   host_addr=127.0.0.1
 fi
 
 echo "Enter host port for whist server (8000):"
 read host_port
-if [ -z "$host_port"]
+if [ -z "$host_port" ]
 then
   host_port=8000
 fi
 
 echo "Enter administrator's username (admin):"
 read admin_name
-if [ -z "$admin_name"]
+if [ -z "$admin_name" ]
 then
   admin_name="admin"
 fi
 
 echo "Enter administrator's password (admin):"
 read admin_pwd
-if [ -z "$admin_pwd"]
+if [ -z "$admin_pwd" ]
 then
   admin_pwd="admin"
 fi

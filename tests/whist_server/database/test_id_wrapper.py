6189b0d1b746ccaf371d63f5dc84d7a9f02883b8
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2023-07-30 21:59:59 +0200

    ADD: test for value error
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist_server/database/test_id_wrapper.py
+++ tests/whist_server/database/test_id_wrapper.py
@@ -11,6 +11,12 @@ def test_pyobjectid_validation(obj):
     ta.validate_python(obj)
 
 
+@pytest.mark.parametrize("obj", ["64b7992ba8f08069073f105", ObjectId("64b7992ba8f08069073f1055")])
+def test_pyobjectid_validation_invalid(obj):
+    ta = TypeAdapter(PyObjectId, config=ConfigDict(arbitrary_types_allowed=True))
+    with pytest.raises(ValueError):
+        ta.validate_python(obj)
+
 @pytest.mark.parametrize("obj", ["64b7992ba8f08069073f1055", ObjectId("64b7992ba8f08069073f1055")])
 def test_pyobjectid_serialization(obj):
     ta = TypeAdapter(PyObjectId, config=ConfigDict(arbitrary_types_allowed=True))
